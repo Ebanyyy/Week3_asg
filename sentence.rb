7a2207856bfafeb7a802fe8ceb299c6adc2c54e1
Ebanyyy <vaniegabu@gmail.com>
2023-09-03 13:58:54 +0800

    sentence

---
+++ sentence.rb
@@ -0,0 +1,13 @@
+class Sentence
+	def initialize
+		@sentence = sentence
+	end
+
+	def sentence
+		@sentence
+	end
+end
+
+words_array = ["My", "name", "is", "Bany"]
+sentence = words_array.join(" ")
+puts sentence
